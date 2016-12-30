00652fc6e84bf6946db475a06fc948f0cc9492f0
ganoush <ganeshatmail@gmail.com>
2016-12-30 01:16:07 -0500

    Throw exception if no file location provided in command line argument

--- src/main/java/com/goeuro/busRoute/BusRouteApplication.java
+++ src/main/java/com/goeuro/busRoute/BusRouteApplication.java
@@ -1,5 +1,6 @@
 package com.goeuro.busRoute;
 
+import com.goeuro.busRoute.exception.BusRouteException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -13,11 +14,13 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 public class BusRouteApplication {
     private static final Logger log = LoggerFactory.getLogger(BusRouteApplication.class);
 
-    public static void main(String args[]){
+    public static void main(String args[]) throws BusRouteException {
         String fileLoc = "";
         if (args.length == 0) {
-            fileLoc = "src/main/resources/BusRoutes.txt";
-            log.info("No Bus route file location passed in the argument. Default file loaded from " + fileLoc);
+            //fileLoc = "src/main/resources/BusRoutes.txt";
+            //log.info("No Bus route file location passed in the argument. Default file loaded from " + fileLoc);
+            log.error("No Bus route file location provided ");
+            throw new BusRouteException("No Bus route file provided. Please pass the location of the file in command line.");
         } else {
             fileLoc = args[0];
             log.info("Bus Route file location loaded from " + fileLoc);
